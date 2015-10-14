7f76d0688697fecd3c31bf5672acd9f551dd734a
Grace Hatamyar <hatamyarg@gmail.com>
2015-10-14 15:41:03 -0700

    change sk and store as env variable

--- lost_and_hound/lost_and_hound/settings.py
+++ lost_and_hound/lost_and_hound/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm*1u0p$1cyv5bl6t50g%)vhwn1#hu2#5(t5_j#$_)(x*r^mdh5'
+SECRET_KEY = os.environ.get('LH_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
