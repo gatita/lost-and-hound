039f351798211faba5e8a0905f21066678138f51
Grace Hatamyar <hatamyarg@gmail.com>
2015-11-05 21:23:38 -0800

    send mpbox key

--- lost_and_hound/lost_and_hound/urls.py
+++ lost_and_hound/lost_and_hound/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from settings import MAPBOX_KEY
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'),
+        {'mapbox_key': MAPBOX_KEY},
         name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]
